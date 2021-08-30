450b434753926faa2e80878935a0bc3d7609e32d
chummypixels <chummypixels@gmail.com>
2021-08-30 07:31:31 +0100

    implement api call

---
+++ lib/main.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/widgets/weather_tile.dart';
+import 'package:http/http.dart' as http;
+import 'widgets/main_widget.dart';
+import 'dart:async';
+import 'dart:convert';
+
+Future<WeatherInfo> fetchWeather() async {
+  final apiKey = 'f3e9abd8bbc67d324baaa6aa8e0732b1';
+  final requestUrl =
+      'https://api.openweathermap.org/data/2.5/weather?q=nsukka&appid=${apiKey}';
+
+  final response = await http.get(Uri.parse(requestUrl));
+
+  if (response.statusCode == 200) {
+    return WeatherInfo.fromJson(jsonDecode(response.body));
+  } else {
+    throw Exception('Error loading request URL info');
+  }
+}
+
+class WeatherInfo {
+  final String location;
+  final double temp;
+  final double tempMin;
+  final double tempMax;
+  final String weather;
+  final int humidity;
+  final double windSpeed;
+
+  WeatherInfo(
+      {required this.location,
+      required this.temp,
+      required this.tempMin,
+      required this.tempMax,
+      required this.weather,
+      required this.humidity,
+      required this.windSpeed});
+
+  factory WeatherInfo.fromJson(Map<String, dynamic> json) {
+    return WeatherInfo(
+        location: json['name'],
+        temp: json['main']['temp'],
+        tempMin: json['main']['temp_min'],
+        tempMax: json['main']['temp_max'],
+        weather: json['weather'][0]['description'],
+        humidity: json['main']['humidity'],
+        windSpeed: json['wind']['speed']);
+  }
+}
+
+void main() => runApp(MaterialApp(
+      title: 'Weather App',
+      home: MyApp(),
+    ));
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+//f3e9abd8bbc67d324baaa6aa8e0732b1
+//api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+class _MyAppState extends State<MyApp> {
+  late Future<WeatherInfo> futureWeather;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    futureWeather = fetchWeather();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: FutureBuilder<WeatherInfo>(
+      future: futureWeather,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return MainWidget(
+              location: snapshot.data!.location,
+              temp: snapshot.data!.temp,
+              tempMin: snapshot.data!.tempMin,
+              tempMax: snapshot.data!.tempMax,
+              weather: snapshot.data!.weather,
+              humidity: snapshot.data!.humidity,
+              windspeed: snapshot.data!.windSpeed);
+        } else if (snapshot.hasError) {
+          return Center(
+            child: Text('${snapshot.error}'),
+          );
+        }
+
+        return CircularProgressIndicator();
+      },
+    ));
+  }
+}
